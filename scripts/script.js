93abb5d0da8bc88b93de79511e848340beb08379
Vlad Solovey <connectwithvlad@gmail.com>
2022-07-24 14:06:15 +0300

    feat: make equality sign work

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,8 @@ const displayResult = document.querySelector(".display-result");
 const displayHistory = document.querySelector(".display-history");
 const buttonContainer = document.querySelector(".button-container")
 
+let firstNumber = 0;
+let operation = "";
 
 function add(num1, num2) {
     return num1 + num2;
@@ -34,19 +36,53 @@ function operate(operator, num1, num2) {
     }
 }
 
+function clearDisplayResult() {
+    displayResult.textContent = "0";
+}
+
+function clearDisplayHistory() {
+    displayHistory.textContent = "";
+}
+
+function clearDisplay() {
+    clearDisplayResult();
+    clearDisplayHistory();
+}
+
+function resetVariables() {
+    firstNumber = 0;
+    operation = "";
+}
+
 function handleNumberInput(target) {
     let value = target.textContent;
     // check for repeating dot
     if (value === "." && displayResult.textContent.includes(".")) return 0;
     // remove preceding zero
-    if (displayResult.textContent === "0") displayResult.textContent = value;
+    if (displayResult.textContent === "0" && value !== ".") displayResult.textContent = value;
     else displayResult.textContent += value;
+}
+
+function handleOperationInput(target) {
+    firstNumber = displayResult.textContent;
+    operation = target.textContent;
+    displayHistory.textContent = firstNumber + " " + operation;
+    clearDisplayResult();
+}
+
+function handleEqualityInput() {
+    if (operation === "") return 0;
+    displayHistory.textContent += displayResult.textContent + " =";
+    displayResult.textContent = operate(operation, +firstNumber, +displayResult.textContent);
+    resetVariables();
 }
 
 function handleEvent(e) {
     if (!e.target.classList.contains("button")) return 0;
     if (e.target.classList.contains("button-number")) handleNumberInput(e.target);
-    
+    if (e.target.classList.contains("button-operation")) handleOperationInput(e.target);
+    if (e.target.classList.contains("button-equality")) handleEqualityInput();
+
     console.log(e.target);
 }
 
